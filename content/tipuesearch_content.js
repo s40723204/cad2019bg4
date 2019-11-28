4d132f25394a076c4c011fde9dc39af208cdc99a
s40723242 <s40723242@gm.nfu.edu.tw>
2019-11-28 22:06:40 +0800

    member42

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'Member', 'text': '\n 40723201 \xa0 \xa0 吳沛汶 \n 40723204 \xa0 \xa0 邱倚瀅 \n 40723213 \xa0 \xa0 杜玗宸 \n 40723215\xa0 \xa0 \xa0 杜彥霆 \n 40723218 \xa0 \xa0 林志豪 \n 40723219 \xa0 \xa0 林承治 \n 40723237 \xa0\xa0 \xa0 黃俊翔 \n 40723242\xa0 \xa0 \xa0趙子德 \n 40723243 \xa0\xa0 \xa0 劉子源\xa0 87 \n 40723245 \xa0\xa0 \xa0 蔡育灃 \n', 'tags': '', 'url': 'Member.html'}]};
-=======
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'Member', 'text': '\n 40723201 \xa0 \xa0 吳沛汶 \n 40723204 \xa0 \xa0 邱倚瀅 \n 40723213 \xa0 \xa0 杜玗宸 \n 40723215\xa0 \xa0 \xa0 杜彥霆 \n 40723218 \xa0 \xa0 林志豪 \n 40723219 \xa0 \xa0 林承治 \n 40723237 \xa0\xa0 \xa0 黃俊翔\xa0 \n 40723242\xa0 \xa0 \xa0趙子德 \n 40723243 \xa0\xa0 \xa0 劉子源 \n 40723245 \xa0\xa0 \xa0 蔡育灃 \n', 'tags': '', 'url': 'Member.html'}]};
-=======
-var tipuesearch = {"pages": [{'title': 'Member', 'text': '\n 40723201 \xa0 \xa0 吳沛汶 \n 40723204 \xa0 \xa0 邱倚瀅 \n 40723213 \xa0 \xa0 杜玗宸 13 \n 40723215\xa0 \xa0 \xa0 杜彥霆 \n 40723218 \xa0 \xa0 林志豪 \n 40723219 \xa0 \xa0 林承治 \n 40723237 \xa0\xa0 \xa0 黃俊翔 \n 40723242\xa0 \xa0 \xa0趙子德 \n 40723243 \xa0\xa0 \xa0 劉子源\xa0\xa0 \n 40723245 \xa0\xa0 \xa0 蔡育灃 \n', 'tags': '', 'url': 'Member.html'}]};
->>>>>>> 538e785622e8b0482b05b9fe2cf220f597a8edc9
->>>>>>> 691411d5baa57088db8210fbde7b1c75e9f789bc
+var tipuesearch = {"pages": [{'title': 'Member', 'text': '\n 40723201 \xa0 \xa0 吳沛汶 \n 40723204 \xa0 \xa0 邱倚瀅 \n 40723213 \xa0 \xa0 杜玗宸\xa0 \n 40723215\xa0 \xa0 \xa0杜彥霆 \n 40723218 \xa0 \xa0 林志豪 \n 40723219 \xa0 \xa0 林承治 \n 40723237 \xa0 \xa0 黃俊翔\xa0 \n 40723242\xa0 \xa0 \xa0趙子德 \n \n 40723243 \xa0 \xa0 劉子源\xa0\xa0 \n 40723245 \xa0 \xa0 蔡育灃 \n', 'tags': '', 'url': 'Member.html'}]};
