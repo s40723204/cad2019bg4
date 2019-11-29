7031babf6f05d893e9c33edfd4739e24e2e3a01f
s40723242 <s40723242@gm.nfu.edu.tw>
2019-11-29 13:43:30 +0800

    member refrush-1

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = 'cad2019bg4'
+SITENAME = 'cad2019bg4 網際內容管理'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
